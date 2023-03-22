e280777dbe37134cafa2d89883bef3f5c70d7be9
Doggy Sasha <sasha.hannarom@ya.ru>
2023-03-22 21:05:53 +0300

    Обновлён установщик

--- XGLauncher Setup/App.xaml.cs
+++ XGLauncher Setup/App.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
